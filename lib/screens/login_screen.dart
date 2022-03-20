c7d50ecfbf806e5df63bf8ec062a7cc2f7ad2ccc
ilaynicholas <ecisungga@up.edu.ph>
2022-03-20 22:16:51 +0800

    removed phone auth temporarily, created login screen UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'home_screen.dart';
-
-enum MobileVerificationState {
-  showMobileFormState,
-  showOtpFormState,
-}
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({ Key? key }) : super(key: key);
@@ -15,157 +8,86 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  MobileVerificationState currentState = MobileVerificationState.showMobileFormState;
-
-  final phoneController = TextEditingController();
-  final otpController = TextEditingController();
-
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  late String verificationId;
-  bool showLoading = false;
-
-  void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) async {
-    setState(() {
-      showLoading = true;
-    });
-
-    try {
-      final authCredential = await _auth.signInWithCredential(phoneAuthCredential);
-
-      setState(() {
-        showLoading = false;
-      });
-
-      if(authCredential.user != null) {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-      }
-    } on FirebaseAuthException catch (e) {
-      setState(() {
-        showLoading = false;
-      });
-      
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text(e.message!),
-        duration: const Duration(seconds: 5)
-      ));
-    }
-  }
-
-  getMobileFormWidget(context) {
-    return Column(
-      children: [
-        const Spacer(),
-        TextField(
-          controller: phoneController,
-          decoration: const InputDecoration(
-            hintText: "+63"
-          ),
-        ),
-        const SizedBox(
-          height: 16,
-        ),
-        ElevatedButton(
-          child: const Text("SEND OTP"),
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              )
-            ),
-            backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF008999))
-          ),
-          onPressed: () async {
-            setState(() {
-              showLoading = true;
-            });
-
-            await _auth.verifyPhoneNumber(
-              phoneNumber: phoneController.text,
-              verificationCompleted: (phoneAuthCredential) async {
-                setState(() {
-                  showLoading = false;
-                });
-              },
-              verificationFailed: (verificationFailed) async {
-                setState(() {
-                  showLoading = false;
-                });
-
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                  content: Text(verificationFailed.message!),
-                  duration: const Duration(seconds: 5)
-                ));
-              },
-              codeSent: (verificationId, resendingToken) async {
-                setState(() {
-                  showLoading = false;
-                  currentState = MobileVerificationState.showOtpFormState;
-                  this.verificationId = verificationId;
-                });
-              },
-              codeAutoRetrievalTimeout: (verificationId) async {}
-            );
-          },
-        ),
-        const Spacer()
-      ]
-    );
-  }
-
-  getOtpFormWidget(context) {
-    return Column(
-      children: [
-        const Spacer(),
-        TextField(
-          controller: otpController,
-          decoration: const InputDecoration(
-            hintText: "Enter OTP"
-          ),
-        ),
-        const SizedBox(
-          height: 16
-        ),
-        ElevatedButton(
-          child: const Text("VERIFY"),
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              )
-            ),
-            backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF008999))
-          ),
-          onPressed: () async {
-            PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(
-              verificationId: verificationId,
-              smsCode: otpController.text
-            );
-
-            signInWithPhoneAuthCredential(phoneAuthCredential);
-          },
-        ),
-        const Spacer()
-      ],
-    );
-  }
-
-  final GlobalKey<ScaffoldMessengerState> _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      body: Container(
-        child: showLoading 
-          ? const Center(
-            child: CircularProgressIndicator()
+    return Material(
+      type: MaterialType.transparency,
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.white, Color(0xFF00CDA6)]
           )
-          : currentState == MobileVerificationState.showMobileFormState
-            ? getMobileFormWidget(context)
-            : getOtpFormWidget(context),
-        padding: const EdgeInsets.all(16)
-      )
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            const Text(
+              "GapanTrax",
+              style: TextStyle(
+                fontSize: 36.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF008999),
+              )
+            ),
+            const Text(
+              "Contact Tracing Application of\nGapan City",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF008999),
+              )
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                  "REGISTER",
+                  style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w600
+                    )
+                ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0)
+                ),
+                primary: const Color(0xFF008999),
+                onPrimary: Colors.white,
+                minimumSize: const Size(300, 60)
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 15.0)),
+            const Text(
+              "Do you own an establishment?",
+              style: TextStyle(
+                color: Colors.white
+              )
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                  "Register as\nEstablishment",
+                  style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  textAlign: TextAlign.center,
+                ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0)
+                ),
+                primary: Colors.white,
+                onPrimary: const Color(0xFF008999),
+                minimumSize: const Size(300, 60)
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(bottom: 120.0))
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/screens/temp_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'home_screen.dart';
+
+enum MobileVerificationState {
+  showMobileFormState,
+  showOtpFormState,
+}
+
+class TempScreen extends StatefulWidget {
+  const TempScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<TempScreen> createState() => _TempScreenState();
+}
+
+class _TempScreenState extends State<TempScreen> {
+  MobileVerificationState currentState = MobileVerificationState.showMobileFormState;
+
+  final phoneController = TextEditingController();
+  final otpController = TextEditingController();
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  late String verificationId;
+  bool showLoading = false;
+
+  void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) async {
+    setState(() {
+      showLoading = true;
+    });
+
+    try {
+      final authCredential = await _auth.signInWithCredential(phoneAuthCredential);
+
+      setState(() {
+        showLoading = false;
+      });
+
+      if(authCredential.user != null) {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+      }
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        showLoading = false;
+      });
+      
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text(e.message!),
+        duration: const Duration(seconds: 5)
+      ));
+    }
+  }
+
+  getMobileFormWidget(context) {
+    return Column(
+      children: [
+        const Spacer(),
+        TextField(
+          controller: phoneController,
+          decoration: const InputDecoration(
+            hintText: "+63"
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        ElevatedButton(
+          child: const Text("SEND OTP"),
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+              )
+            ),
+            backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF008999))
+          ),
+          onPressed: () async {
+            setState(() {
+              showLoading = true;
+            });
+
+            await _auth.verifyPhoneNumber(
+              phoneNumber: phoneController.text,
+              verificationCompleted: (phoneAuthCredential) async {
+                setState(() {
+                  showLoading = false;
+                });
+              },
+              verificationFailed: (verificationFailed) async {
+                setState(() {
+                  showLoading = false;
+                });
+
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text(verificationFailed.message!),
+                  duration: const Duration(seconds: 5)
+                ));
+              },
+              codeSent: (verificationId, resendingToken) async {
+                setState(() {
+                  showLoading = false;
+                  currentState = MobileVerificationState.showOtpFormState;
+                  this.verificationId = verificationId;
+                });
+              },
+              codeAutoRetrievalTimeout: (verificationId) async {}
+            );
+          },
+        ),
+        const Spacer()
+      ]
+    );
+  }
+
+  getOtpFormWidget(context) {
+    return Column(
+      children: [
+        const Spacer(),
+        TextField(
+          controller: otpController,
+          decoration: const InputDecoration(
+            hintText: "Enter OTP"
+          ),
+        ),
+        const SizedBox(
+          height: 16
+        ),
+        ElevatedButton(
+          child: const Text("VERIFY"),
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(18.0),
+              )
+            ),
+            backgroundColor: MaterialStateProperty.all<Color>(const Color(0xFF008999))
+          ),
+          onPressed: () async {
+            PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(
+              verificationId: verificationId,
+              smsCode: otpController.text
+            );
+
+            signInWithPhoneAuthCredential(phoneAuthCredential);
+          },
+        ),
+        const Spacer()
+      ],
+    );
+  }
+
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Container(
+        child: showLoading 
+          ? const Center(
+            child: CircularProgressIndicator()
+          )
+          : currentState == MobileVerificationState.showMobileFormState
+            ? getMobileFormWidget(context)
+            : getOtpFormWidget(context),
+        padding: const EdgeInsets.all(16)
+      )
+    );
+  }
+}
